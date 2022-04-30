f9914aaf73bcf2db99a2531a5aca1d8419b86e1a
SpeedyNS <63477484+GamingSlayerNS@users.noreply.github.com>
2022-04-29 19:19:22 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Runtime;
 public class Main
 {
-    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello_World!");
+    }
 }
